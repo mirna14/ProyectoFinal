dc920ccede3938c383ecc8c10d48e01113048037
mirna14 <95093657+mirna14@users.noreply.github.com>
2023-06-12 09:12:33 +0000

    Conf Completa

--- server/app.js
+++ server/app.js
@@ -14,7 +14,48 @@ import indexRouter from '@server/routes/index'
 import usersRouter from '@server/routes/users';
 import apiRouter from '@server/routes/api';
 
+// Setting Webpack Modules
+import webpack from 'webpack';
+import WebpackDevMiddleware from 'webpack-dev-middleware';
+import WebpackHotMiddleware from 'webpack-hot-middleware';
+// Importing webpack configuration
+import webpackConfig from '../webpack.dev.config';
+
 const app = express();
+
+
+//get the execution node
+const nodeEnviroment = process.env.NODE_ENV || 'production'
+
+// Deciding if we add webpack middleware or not
+if(nodeEnviroment === 'development'){
+  // Start Webpack dev server
+  console.log("🛠️  Ejecutando en modo desarrollo");
+  // Adding the key "mode" with its value "development"
+  webpackConfig.mode = nodeEnviroment;
+  // Setting the dev server port to the same value as the express server
+  webpackConfig.devServer.port = process.env.PORT;
+  // Setting up the HMR (Hot Module Replacement)
+  webpackConfig.entry = [
+    "webpack-hot-middleware/client?reload=true&timeout=1000",
+    webpackConfig.entry
+  ];
+	// Agregar el plugin a la configuración de desarrollo
+  // de webpack
+  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+  // Creating the bundler
+  const bundle = webpack(webpackConfig);
+  // Enabling the webpack middleware
+  app.use( WebpackDevMiddleware(bundle, {
+    publicPath: webpackConfig.output.publicPath
+    
+  }) );
+  //  Enabling the webpack HMR
+  app.use( WebpackHotMiddleware(bundle) );
+}else{
+  console.log("🏭 Ejecutando en modo producción 🏭");
+}
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +66,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/',indexRouter);
 app.use('/users',usersRouter);
