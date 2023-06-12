0984da0b333a4789ff5cc62d05391c330bef00c0
mirna14 <95093657+mirna14@users.noreply.github.com>
2023-06-12 08:15:26 +0000

    ✨webpack en el front-end

---
+++ client/index.js
@@ -0,0 +1,27 @@
+console.log("🎉 Client Server working powered by Webpack 🎉");
+
+// ES6 CODE
+// Default Parameters
+let show = (msg="No message given") => {
+  console.log(msg)
+}
+
+show();
+
+// ES6 CODE
+// Async Await
+function resolveAfter2Seconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000)
+  });
+}
+
+async function asyncCall() {
+  console.log("Calling");
+  const result = await resolveAfter2Seconds();
+  console.log(result);
+}
+
+asyncCall();
