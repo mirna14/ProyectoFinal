c55f180fc3999d7ac71caa8096d39621317c754c
mirna14 <95093657+mirna14@users.noreply.github.com>
2023-06-12 06:46:59 +0000

    Corriendo hasta babel

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,19 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express with babel' });
+  let iconSet = ["⭐", "🤖", "🍉"];
+  let icon = iconSet[Math.floor(Math.random() * 3)]
+  res.render('index', { title: 'DWPCII-2023A', icon });
+});
+
+router.get('/author', (req, res) => {
+  let author = {
+    "name": "Mirna",
+    "lastname": "Sanchez",
+    "twitter": "@Mirna",
+    "job": "ITGAM"
+  }
+
 });
 
 module.exports = router;
